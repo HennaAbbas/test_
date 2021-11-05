955f8eb52af7b18a43ade4a647b5a52f7801760c
Henna <62267575+HennaAbbas@users.noreply.github.com>
2021-11-05 13:54:49 -1000

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,38 +1,3 @@
 #!/bin/bash -e
 
-FAILURE=1
-
-export APP_NAME="node-expel"
-export APP_VERSION="$(git describe --tags 2>/dev/null)"
-
-
-usage() {
-   echo 'Usage: ./ci.sh <make target>'
-   echo 'Make targets:'
-   echo
-   for i in $(awk '/^\w+:/ {gsub(":", ""); print $1}' Makefile); do
-       echo $i ;
-   done
-   echo
-}
-
-could_not_acquire_tag() {
-    echo Fatal: Could not acquire git tag
-    exit $FAILURE
-}
-
-validate() {
-  if [ $# -ne 1 ]; then
-      usage
-      exit $FAILURE
-  fi
-
-  if [ -z $APP_VERSION ]; then
-      could_not_acquire_tag
-  fi
-}
-
-MAKE_TARGET="$1"
-
-validate "$@"
-docker-compose run $APP_NAME $MAKE_TARGET
+return "hello world"
