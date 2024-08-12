2b7484334347f7348c773b922fc9c6c619843355
Henna <62267575+HennaAbbas@users.noreply.github.com>
2024-08-12 15:17:25 -0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,14 @@
+import time
+
+# Set the number of log lines you want to generate
+num_log_lines = 1000
+
+for i in range(50000):
+    # Log format with a timestamp and log message
+    log_message = f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Log message {i}"
+    
+    # Print the log message to the console
+    print(log_message)
+    
+    # You can add a delay if you want to simulate real-time logging
+    # time.sleep(0.1)  # Uncomment this line for a delay between log entries
